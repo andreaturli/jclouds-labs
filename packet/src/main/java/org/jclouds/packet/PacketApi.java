cc25801f67a2a772747f0a0133dc16761a97d06b
Andrea Turli <andrea.turli@gmail.com>
2016-09-15 12:31:58 +0200

    setup project
    - create skeleton
    - create authentication filter
    - smoke tests

---
+++ packet/src/main/java/org/jclouds/packet/PacketApi.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jclouds.packet;
+
+import java.io.Closeable;
+
+import org.jclouds.packet.features.FacilityApi;
+import org.jclouds.packet.features.ProjectApi;
+import org.jclouds.packet.filters.AddXAuthTokenToRequest;
+import org.jclouds.rest.annotations.Delegate;
+import org.jclouds.rest.annotations.RequestFilters;
+
+/**
+ * The Packet API is a REST API for managing your services and deployments.
+ * <p>
+ *
+ * @see <a href="https://www.packet.net/help/api/" >doc</a>
+ */
+public interface PacketApi extends Closeable {
+
+   /**
+    * The Packet API includes operations for managing project.
+    *
+    * @see <a href="https://www.packet.net/help/api/#page:projects,header:projects-projects">docs</a>
+    */
+   @Delegate
+   ProjectApi getProjectApi();
+
+   /**
+    * This Packet API provides all of the facilities
+    *
+    * @see <a href="https://www.packet.net/help/api/#page:devices,header:devices-locations">docs</a>
+    */
+   @Delegate
+   FacilityApi getFacilityApi();
+
+}
